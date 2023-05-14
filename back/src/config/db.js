27ccba983df99e3cc03caad652b5eba59d685980
DMKuldeep <tkuldeep271@gmail.com>
2023-05-14 17:42:42 +0530

    first commit

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -1,6 +1,10 @@
-
 const mongoose = require("mongoose");
 
-module.exports = ()=>{
-   return mongoose.connect( "mongodb+srv://user:kush123@cluster0.4xk9u.mongodb.net/blackcoffee?retryWrites=true&w=majority");
-}
+module.exports = () => {
+  return mongoose.connect(
+    "mongodb+srv://user:kush123@cluster0.4xk9u.mongodb.net/blackcoffee?retryWrites=true&w=majority"
+  );
+  // return mongoose.connect(
+  //   "mongodb+srv://kuldeep:kuldeep@cluster0.vv9fmep.mongodb.net/blackcoffer?retryWrites=true&w=majority"
+  // );
+};
--- back/src/controllers/api.js
+++ back/src/controllers/api.js
@@ -1,42 +1,39 @@
-
 const express = require("express");
 const profile = require("../models/prod");
-
+var cors = require("cors");
+const app = express();
+app.use(cors());
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-   
     const page = req.query.page || 1;
     const size = req.query.size || 6;
-    const showArt=await profile.find()
-    .skip((page -1)*size)
-    .limit(size)
-    .lean().exec();
+    const showArt = await profile
+      .find()
+      .skip((page - 1) * size)
+      .limit(size)
+      .lean()
+      .exec();
 
     const totalPages = Math.ceil(
-        (await profile.find().countDocuments())/size
+      (await profile.find().countDocuments()) / size
     );
 
-    return res.send({showArt,totalPages});
-
+    return res.send({ showArt, totalPages });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
   }
-  catch (err) {
-      return res.status(500).send({ message: err.message })
-  }
-})
-
+});
 
 router.post("", async (req, res) => {
   try {
-   
-    let ofile = await profile.create(req.body)
-    return res.status(200).send(ofile)
+    let ofile = await profile.create(req.body);
+    return res.status(200).send(ofile);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
   }
-  catch (err) {
-      return res.status(500).send({ message: err.message })
-  }
-})
+});
 
-module.exports = router
+module.exports = router;
